6186e34c2d5b6f7d61d2b089993748372dbba3c2
elinapark0818 <elinapark0818@gmail.com>
2021-11-09 11:54:34 +0900

    add function and for

---
+++ study/function.js
@@ -0,0 +1,22 @@
+// 함수
+
+// 함수 선언식
+function getRectangleArea(width, height) {
+    let rectangleArea = width * height;
+    return rectangleArea;
+}
+
+// 함수 표현식
+let getRectangleArea = function (width, height) {
+    let rectangleArea = width * height;
+    return rectangleArea
+}
+
+// 화살표 함수 1
+let getRectangleArea = (width, height) => {
+    let rectangleArea = width * height;
+    return rectangleArea
+}
+
+// 화살표 함수 2
+let getRectangleArea = (width, height) => width * height
---
+++ study/gugudan.js
@@ -0,0 +1,7 @@
+// 구구단
+for(let i=2; i<=9; i++) {
+    console.log(i+'단')
+    for(let j=1; j<=9; j++) {
+        console.log(i + '*' + j + '=' + (i*j))
+    }
+}
