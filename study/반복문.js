9f23f7dfefae7be5a74fb4994204c2819de8bd40
elinapark0818 <elinapark0818@gmail.com>
2021-11-12 02:16:34 +0900

    반복문 기초

---
+++ study/반복문.js
@@ -0,0 +1,25 @@
+function findTheBug(word) {
+
+if (word.includes('#')) {
+    for (let i=0; i<word.length; i++) {
+    return word.indexOf('#') 
+    }
+}
+else {
+    return undefined}
+}
+
+
+// ---
+function makePermutations(str) {  // 'cat'
+    let result = '';
+    for (let i=0; i<str.length; i++) {
+        for (let j=0; j<str.length; j++) {
+            result = result + `${str[i]}${str[j]},`;
+        }
+    }
+    return result.slice(0, result.length - 1)
+}
+
+makePermutations('cat')
+
