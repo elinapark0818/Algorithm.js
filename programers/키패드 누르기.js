7f3ee2dcc4a8ed6c8bc23f4cbe9a1c1f16d6f5e4
elina <elinapark0818@gmail.com>
2021-11-17 21:29:08 +0900

    Add another solution

--- programers/키패드 누르기.js
+++ programers/키패드 누르기.js
@@ -73,3 +73,54 @@ function distance(start, target) {
     sum += Math.max(start[1], target[1]) - Math.min(start[1], target[1]);
     return sum;
 }
+
+// 다른 풀이
+
+function solution(numbers, hand) {
+    // hand는 'left' 또는 'right' 이다.  왼손잡이 or 오른손잡이
+    hand = hand[0] === 'r' ? 'R' : 'L'
+    // 1 은 키패드 '0' 
+    // 4 는 키패드 '1', '2', '3' 
+    // 3 은 키패드 '4', '5', '6'
+    // 2 는 키패드 '7', '8', '9'
+    let position = [1, 4, 4, 4, 3, 3, 3, 2, 2, 2]
+    // 처음 손가락의 위치
+    let thumb = { L:[1, 1], R :[1, 1] }
+    // map()과 화살표 함수를 사용해서 위방향으로만 움직일 때
+    // 정규식 test()를 사용해서 true 일 경우
+    // 왼손엄지와 가까운 키패드 '1', '4', '7' 은 'L'을 리턴하게 한다
+    // 오른손엄지와 가까운 키패드 '3', '6', '9' 는 'R'을 리턴하게 한다
+    return numbers.map(i => {
+        if (/[147]/.test(i)) {
+            thumb.L = [ position[i], 1 ]
+            return 'L'
+        }
+        if (/[369]/.test(i)) {
+            thumb.R = [ position[i], 1 ]
+            return 'R'
+        }
+        // 왼손엄지에서부터의 거리와 오른손 엄지에서부터의 거리를
+        // Math.abs()를 사용해서 절대값으로 구한다.
+        let distanceLeft = Math.abs(position[i] - thumb.L[0]) + thumb.L[1]
+        let distanceRight = Math.abs(position[i] - thumb.R[0]) + thumb.R[1]
+        // 거리가 같을 경우 왼손잡이인지 오른손잡이인지 확인해서 리턴하게 한다
+        if (distanceLeft === distanceRight) {
+            thumb[hand] = [position[i], 0]
+            return hand
+        }
+        // distanceLeft 가 distanceRight 보다 작은 경우, 'L'을 리턴한다
+        // distanceRight 가 distanceLeft 보다 작은 경우, 'R'을 리턴한다
+        if ( distanceLeft < distanceRight ) {
+            thumb.L = [position[i], 0]
+            return 'L'
+        } else {
+            thumb.R = [position[i], 0]
+            return 'R'
+        }
+    // join()을 사용해서 배열을 합친다.
+    }).join("");
+}
+
+// 정규식 test() 메소드
+// 주어진 문자열이 정규식에 만족하는 지 판별하고, 그 여부는 Boolean 값으로 반환한다
+// 만족하면 true, 아니면 false를 반환한다
