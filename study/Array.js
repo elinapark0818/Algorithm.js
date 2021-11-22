36aeff0f20ada2acfd58096cd3453808983b1ab8
elina <elinapark0818@gmail.com>
2021-11-22 14:08:30 +0900

    Find a frequentChar in str

--- study/Array.js
+++ study/Array.js
@@ -1,128 +1,128 @@
-// 배열인지 아닌지 확인하기 위해서는 Array.isArray() 
+// 배열인지 아닌지 확인하기 위해서는 Array.isArray()
 // 배열은 참조형 자료형이기 때문에 arr = [], arr !== [] 이다
 // 길이에서 1을 뺀 값이 마지막 요소이다 length - 1
 // 빈 배열인지 확인하기 위해서는 arr.length === 0
 // 배열 안에서 더하는 함수를 만드는 방법은 arr.reduce((a, b) => (a + b))
-// split 으로 정한 기준에 따라 끊고, 원하는 개수만큼 반환할 수 있다. 
-// str.split(' ', str.length) 
+// split 으로 정한 기준에 따라 끊고, 원하는 개수만큼 반환할 수 있다.
+// str.split(' ', str.length)
 // 문자열을 공백으로 끊고, 처음부터 문자열의 길이만큼의 개수를 문자열로 반환한다
-// const newObject = [...object] 기존 object를 복제하여 할당 할 수 있다. 
-// 숫자로 이루어진 배열의 요소 중 가장 큰 값을 구하기 위해서는  arr.reduce((a, b) => Math.max(a, b)) 또는 Math.max(...arr) 
+// const newObject = [...object] 기존 object를 복제하여 할당 할 수 있다.
+// 숫자로 이루어진 배열의 요소 중 가장 큰 값을 구하기 위해서는  arr.reduce((a, b) => Math.max(a, b)) 또는 Math.max(...arr)
 // for of 로 간단하게 반복문 구현이 가능하다
 // arr.slice(start, end(미포함)) 기존 배열을 복제하여 새로운 배열로 반환한다
 // arr.slice() 배열을 그대로 복제해온다
 // arr.slice(1) 인 경우 첫번째 요소를 제외한 배열을 만들 수 있다
-// arr.slice(0, arr.length -1) 마지막 요소를 제외한 배열을 만들 수 있다
+// arr.slice(0, arr.length) 마지막 요소를 제외한 배열을 만들 수 있다
 // arr.concat(anotherArr) 배열이나 값들을 기존 배열에 합쳐서 새 배열을 반환한다.
 // push, pop, unshift, shift 는 기존 배열 내에서 요소를 추가/삭제 할 때 사용한다
 // arr.join('') 배열을 또는 배열의 특정요소를 string 타입으로 반환한다
-
+// arr.splice(0) 원본 배열을 리턴한다
+// arr.splice() 빈 배열을 리턴한다
 
 function fibonacci(num) {
-    // TODO: 여기에 코드를 작성합니다.
-    let result = [0, 1]; // 0번째 피보나치 수는 0이고, 1번째 피보나치는 1이다.
+  // TODO: 여기에 코드를 작성합니다.
+  let result = [0, 1]; // 0번째 피보나치 수는 0이고, 1번째 피보나치는 1이다.
 
-    if (!num) {
-        return [0]
-    }
-    
-    for (let i=2; i<=num; i++) { 
-      // 2번째 피보나치 수부터는! 반복문으로 구하자.
-      result.push(result[i-2] + result[i-1]); // n-2번째 피보나치 수와 n-1번째 피보나치 수를 합한 결과다.
-    }
-    return result
+  if (!num) {
+    return [0];
+  }
+
+  for (let i = 2; i <= num; i++) {
+    // 2번째 피보나치 수부터는! 반복문으로 구하자.
+    result.push(result[i - 2] + result[i - 1]); // n-2번째 피보나치 수와 n-1번째 피보나치 수를 합한 결과다.
+  }
+  return result;
 }
-
 
 // 배열 메소드
 
 // .concat : 두 개이상의 배열을 병합할 때 사용한다. 기존 배열을 변경하지 않고, 새 배열을 반환한다
-const arr1 = ['a', 'b', 'c', 'd']
-const arr2 = ['f']
-arr1.concat(arr2)   //['a', 'b', 'c', 'd', 'f']
+const arr1 = ["a", "b", "c", "d"];
+const arr2 = ["f"];
+arr1.concat(arr2); //['a', 'b', 'c', 'd', 'f']
 
 // .copyWithin(target, start, end) : 배열의 일부를 동일한 배열의 다른 위치에 복사하고 크기를 수정하지 않고 반환한다
-const arr3 = ['z', 'x', 'c', 'v']
-arr3.copyWithin(0, 2)    // 0번째에 있는 요소를 2번째에 있는 요소로 바꾼다
+const arr3 = ["z", "x", "c", "v"];
+arr3.copyWithin(0, 2); // 0번째에 있는 요소를 2번째에 있는 요소로 바꾼다
 // end 값은 조금 이해가 잘 안된다. (요소 복사를 종료할 0부터 시작하는 인덱스)
 
 // .entries() : 배열의 각 인덱스에 대한 키/값 쌍을 포함하는 새로운 배열을 반환한다
-const arr4 = ['q', 1, 'w', 2]
+const arr4 = ["q", 1, "w", 2];
 const arrEntries = arr4.entries();
-console.log(arrEntries.next().value)    // [0, 'q']
-console.log(arrEntries.next().value)    // [1, 1]
-console.log(arrEntries.next().value)    // [2, 'w']
-console.log(arrEntries.next().value)    // [3, 2]
+console.log(arrEntries.next().value); // [0, 'q']
+console.log(arrEntries.next().value); // [1, 1]
+console.log(arrEntries.next().value); // [2, 'w']
+console.log(arrEntries.next().value); // [3, 2]
 
 // .every() : 배열의 모든 요소가 제공된 함수에 의해 구현된 테스트를 통과하는지 여부를 테스트한다
 function func(value) {
-    return value > 10;
+  return value > 10;
 }
 const arr5 = [1, 5, 15, 30];
-arr5.every(func)    // false
+arr5.every(func); // false
 const arr6 = [50, 85, 30, 20];
-arr6.every(func)    // true
+arr6.every(func); // true
 
 // .fill(value, start, end) : 시작 인덱스에서 끝 인덱스까지 배열의 모든 요소를 value 로 채운다.(정적 값으로)
 // start 에서 end 전까지(end 미포함) value로 채운다
 const arr7 = [0, 0, 0, 0, 0];
-arr7.fill(1, 1)     // [0, 1, 1, 1, 1]
-arr7.fill(2, 2, 3)  // [0, 0, 2, 0, 0]
-arr7.fill(3, 4, 5)  // [0, 0, 0, 0, 3]
-arr7.fill(3, 0)     // [3, 3, 3, 3, 3]
-arr7.fill(0)        // [0, 0, 0, 0, 0]
+arr7.fill(1, 1); // [0, 1, 1, 1, 1]
+arr7.fill(2, 2, 3); // [0, 0, 2, 0, 0]
+arr7.fill(3, 4, 5); // [0, 0, 0, 0, 3]
+arr7.fill(3, 0); // [3, 3, 3, 3, 3]
+arr7.fill(0); // [0, 0, 0, 0, 0]
 
 // .filter() : 제공된 함수에 의해 구현된 테스트를 통과하는 모든 요소가 포함된 새 배열을 만든다
-const arr8 = ['apple', 'queen', 'fox', 'tree']
-const arrFilter = arr8.filter(word => word.length > 4) // ['apple', 'queen']
+const arr8 = ["apple", "queen", "fox", "tree"];
+const arrFilter = arr8.filter((word) => word.length > 4); // ['apple', 'queen']
 
 // .find() : 제공된 테스트 기능을 충족하는 배열의 첫 번째 요소 값을 반환한다. 그렇지 않으면 undefined가 반환한다
-const arr9 = [10, 20, 30, 40, 50]
-const arrFind = arr9.find(function(element) {
-    return element > 30
-})  // 40
+const arr9 = [10, 20, 30, 40, 50];
+const arrFind = arr9.find(function (element) {
+  return element > 30;
+}); // 40
 
 // .findIndex() : 제공된 테스트 기능을 만족하는 배열의 첫 번째 요소의 인덱스를 반환한다. 그렇지 않으면 -1이 반환한다
-const arr10 = [10, 20, 30, 40, 50]
+const arr10 = [10, 20, 30, 40, 50];
 function findNumIndex(el) {
-    return el > 30
+  return el > 30;
 }
-arr10.findIndex(findNumIndex)   // 3
+arr10.findIndex(findNumIndex); // 3
 
 // .flat() : 지정된 깊이까지 재귀적으로 연결된 모든 하위 배열 요소로 새 배열을 만든다
-const arr11 = [1, 2, [3, 4, [5, 6]]]
-arr11.flat()    // [1, 2, 3, 4, [5, 6]]
-arr11.flat(2)   // [1, 2, 3, 4, 5, 6]
-const arr12 = [1, [2, [3, [4, [5, [6]]]]]]
-arr12.flat(Infinity)    // [1, 2, 3, 4, 5, 6]
+const arr11 = [1, 2, [3, 4, [5, 6]]];
+arr11.flat(); // [1, 2, 3, 4, [5, 6]]
+arr11.flat(2); // [1, 2, 3, 4, 5, 6]
+const arr12 = [1, [2, [3, [4, [5, [6]]]]]];
+arr12.flat(Infinity); // [1, 2, 3, 4, 5, 6]
 
 // .forEach() : 배열 요소당 한 번씩 제공된 함수를 실행한다
-const arr13 = ['안녕?', '응 안녕', '그래']
-arr13.forEach(function(el) {
-    console.log(el);
-})
+const arr13 = ["안녕?", "응 안녕", "그래"];
+arr13.forEach(function (el) {
+  console.log(el);
+});
 // 안녕?
 // 응 안녕
 // 그래
 
 // .include() : 배열에 특정 요소가 포함되어 있는지 확인하여 true 또는 false를 반환한다
-const arr14 = ['Mango', 'Bori', 'Dong2']
-arr14.includes('Mango')  // true
-arr14.includes('아봉이') // false
+const arr14 = ["Mango", "Bori", "Dong2"];
+arr14.includes("Mango"); // true
+arr14.includes("아봉이"); // false
 
 // .indexOf() : 배열에서 주어진 요소를 찾을 수 있는 첫 번째 인덱스를 반환하거나, 존재하지 않는 경우 -1을 반환한다
-arr14.indexOf('Bori')   // 1
-arr14.indexOf('개냥이') // -1
+arr14.indexOf("Bori"); // 1
+arr14.indexOf("개냥이"); // -1
 
 // .join() :  배열의 모든 요소를 ​​문자열로 결합한다
-const arr15 = ['나는','훌륭한','개발자가','될거야']
-arr15.join(' ')  // '나는 훌륭한 개발자가 될거야'
+const arr15 = ["나는", "훌륭한", "개발자가", "될거야"];
+arr15.join(" "); // '나는 훌륭한 개발자가 될거야'
 
 // .keys() : 배열의 각 인덱스에 대한 키를 포함하는 새 배열 반복기를 반환한다
-const arr16 = ['a', 'b', 'c', 'd']
-const arrKeys = arr16.keys()
+const arr16 = ["a", "b", "c", "d"];
+const arrKeys = arr16.keys();
 for (let key of arrKeys) {
-    console.log(key);
+  console.log(key);
 }
 // 0
 // 1
@@ -130,90 +130,93 @@ for (let key of arrKeys) {
 // 3
 
 // .lastIndexOf() : 배열에서 주어진 요소를 찾을 수 있는 마지막 인덱스를 반환하거나 존재하지 않는 경우 -1을 반환한다
-const arr17 = ['오구', '아기오구', '오구', '뚜지']
-arr17.lastIndexOf('오구')   // 2
-arr17.lastIndexOf('비노')   // -1
+const arr17 = ["오구", "아기오구", "오구", "뚜지"];
+arr17.lastIndexOf("오구"); // 2
+arr17.lastIndexOf("비노"); // -1
 
 // .map() : 이 배열의 모든 요소에 대해 제공된 함수를 호출한 결과를 새 배열을 만든다
-const arr18 = [0,1,2,3]
-const arrMap = arr18.map(x => x*3)  // [0, 3, 6, 9]
+const arr18 = [0, 1, 2, 3];
+const arrMap = arr18.map((x) => x * 3); // [0, 3, 6, 9]
 
 // .pop() : 배열에서 마지막 요소를 제거하고 해당 요소를 반환한다. (배열의 길이가 변경된다)
-const arr19 = ['a', 'b', 'c', 'd']  // arr19.length = 4
-arr19.pop()  // 'd' 가 출력된다. arr19.length = 3
+const arr19 = ["a", "b", "c", "d"]; // arr19.length = 4
+arr19.pop(); // 'd' 가 출력된다. arr19.length = 3
 
 // .push() : 배열 끝에 하나 이상의 요소를 추가하고 배열의 새 길이를 반환한다
-const arr20 = ['A', 'B', 'C']
-arr20.push('H')  // 4 가 출력된다. ['A', 'B', 'C', 'H']
+const arr20 = ["A", "B", "C"];
+arr20.push("H"); // 4 가 출력된다. ['A', 'B', 'C', 'H']
 
 // .reduce() : 누산기와 배열의 각 값(왼쪽에서 오른쪽으로)에 대해 함수를 적용하여 단일 값으로 줄인다 (뭔 말이야 이게...)
-const arr21 = [1, 2, 3, 4]
+const arr21 = [1, 2, 3, 4];
 const arrReduce = (accumulator, currentValue) => accumulator + currentValue;
 
-arr21.reduce(arrReduce)  // 10 (1+2+3+4)
-arr21.reduce(arrReduce, 10)  // 20 (10(1+2+3+4) + 10)
+arr21.reduce(arrReduce); // 10 (1+2+3+4)
+arr21.reduce(arrReduce, 10); // 20 (10(1+2+3+4) + 10)
 
 // .reduceRight() : 누산기에 대해 함수를 적용하고 배열의 각 값(오른쪽에서 왼쪽으로)은 이를 단일 값으로 줄인다
-const arr22 = [[0, 1], [2, 3], [4, 5]].reduceRight(
-    (accumulator, currentValue) => accumulator.concat(currentValue));
-console.log(arr22);  // [4, 5, 2, 3, 0, 1]
+const arr22 = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+].reduceRight((accumulator, currentValue) => accumulator.concat(currentValue));
+console.log(arr22); // [4, 5, 2, 3, 0, 1]
 
 // .reverse() : 배열을 제자리에서 뒤집는다
-const arr23 = ['One', 'Two', 'Three']
-arr23.reverse()  // ['Three', 'Two', 'One']
+const arr23 = ["One", "Two", "Three"];
+arr23.reverse(); // ['Three', 'Two', 'One']
 
 // .shift() : 배열에서 첫 번째 요소를 제거하고 해당 요소를 반환한다.(길이가 변경된다)
-const arr24 = [1, 2, 3]
-const arrShift = arr24.shift()
-console.log(arrShift);  // 1
-console.log(arr24);  // [2, 3]
+const arr24 = [1, 2, 3];
+const arrShift = arr24.shift();
+console.log(arrShift); // 1
+console.log(arr24); // [2, 3]
 
 // .slice() : 처음부터 끝까지(끝은 미포함) 선택된 새 배열 객체로 배열의 일부를 얕은 복사본으로 반환한다
-const arr25 = ['ant', 'bison', 'camel', 'duck', 'elephant']
-arr25.slice(1)  // ['bison', 'camel', 'duck', 'elephant']
-arr25.slice(2,4)  // ['camel', 'duck']
+const arr25 = ["ant", "bison", "camel", "duck", "elephant"];
+arr25.slice(1); // ['bison', 'camel', 'duck', 'elephant']
+arr25.slice(2, 4); // ['camel', 'duck']
 
 // .some() : 배열의 일부 요소가 제공된 함수에 의해 구현된 테스트를 통과하는지 여부를 테스트한다
-const arr26 = [20, 45, 5, 89]
-const odd = function(el) {
-    return el % 2 !== 0
-}
-arr26.some(odd)  // true
+const arr26 = [20, 45, 5, 89];
+const odd = function (el) {
+  return el % 2 !== 0;
+};
+arr26.some(odd); // true
 
 // .sort() : 배열의 요소를 제자리에 정렬하고 배열을 반환한다(유니코드 코드 포인트)
-const arr27 = ['A', 'a', 'V', 'z']
-arr27.sort()  // ['A', 'V', 'a', 'z']
+const arr27 = ["A", "a", "V", "z"];
+arr27.sort(); // ['A', 'V', 'a', 'z']
 const arr28 = [1, 30, 4, 21, 100000];
-arr28.sort()  // [1, 100000, 21, 30, 4]
+arr28.sort(); // [1, 100000, 21, 30, 4]
 
 // .splice() : 기존 요소를 제거하거나 새 요소를 추가하여 기존 배열의 내용을 변경한다
 // 요거 연습 좀 해야겠다
 // .splice (start, deleteCount, item)
-const arr29 = ['Jan', 'March', 'April', 'June'];
+const arr29 = ["Jan", "March", "April", "June"];
 // index 1 위치에 있는 0개 지우고, index 1 위치에 'Feb' 추가한다
-arr29.splice(1, 0, 'Feb')  // ['Jan', 'Feb', 'March', 'April', 'June']
+arr29.splice(1, 0, "Feb"); // ['Jan', 'Feb', 'March', 'April', 'June']
 // index 4 위치에 있는 1개의 요소를 지우고 index 4 위치에 'May'를 추가한다
-arr29.splice(4, 1, 'May')  // ['Jan', 'Feb', 'March', 'April', 'May']
+arr29.splice(4, 1, "May"); // ['Jan', 'Feb', 'March', 'April', 'May']
 
 // .toLocaleString : 배열의 요소를 나타내는 문자열을 반환한다.
-const arr30 =  ['오호 이렇게 쓰는군', new Date('19 Nov 2021 23:02:40 UTC')];
-const localeString = arr30.toLocaleString('ko', {timeZone: "UTC"});
-console.log(localeString);  // 오호 이렇게 쓰는군,2021. 11. 19. 오후 11:02:40
+const arr30 = ["오호 이렇게 쓰는군", new Date("19 Nov 2021 23:02:40 UTC")];
+const localeString = arr30.toLocaleString("ko", { timeZone: "UTC" });
+console.log(localeString); // 오호 이렇게 쓰는군,2021. 11. 19. 오후 11:02:40
 
 // .toString() : 지정된 배열과 해당 요소를 나타내는 문자열을 반환한다
-const arr31 = ['카레', '짜장', '짬뽕']
-arr31.toString()  // '카레,짜장,짬뽕'
+const arr31 = ["카레", "짜장", "짬뽕"];
+arr31.toString(); // '카레,짜장,짬뽕'
 
 // .unshift() : 배열의 시작 부분에 하나 이상의 요소를 추가하고 배열의 새 길이를 반환한다
-const arr32 = [5, 6, 7]
-arr32.unshift(3, 4)  // 5 를 출력한다
+const arr32 = [5, 6, 7];
+arr32.unshift(3, 4); // 5 를 출력한다
 console.log(arr32); // [3, 4, 5, 6, 7]
 
 // .value() : 배열의 각 인덱스에 대한 값을 포함하는 새 Array Iterator 객체를 반환한다
-const arr33 = ['헤이', '마마']
-const arrValues = arr33.values()  // Array Iterator 
+const arr33 = ["헤이", "마마"];
+const arrValues = arr33.values(); // Array Iterator
 for (value of arrValues) {
-    console.log(value);
+  console.log(value);
 }
 // 헤이
 // 마마
--- study/Object.js
+++ study/Object.js
@@ -1,131 +1,227 @@
+// 함수에서 객체에 키 값을 할당할 때,
+// 인자로 받아오는 값이 변수로 선언된 내용이기 때문에,
+// 변수에 키값을 할당하기 위해서는 obj[변수명] = value 으로 키값을 할당한다
+// 객체의 반복문은 for in
+// 배열의 반복문은 for of
+
+function frequentChar(str) {
+  // TODO: 여기에 코드를 작성합니다.
+  // 가장 많이 반복되고, 해당숫자에 먼저 도달한 문자
+  let frequentChar = "";
+  // 두개의 객체를 입력받아야 함으로, 빈 객체를 선언
+  let obj = {};
+  // 가장 많이 반복된 횟수를 할당 할 변수
+  let maxCount = 0;
+  // 빈 문자열을 입력받았다면 빈 문자열을 리턴
+  if (str.length === 0) {
+    return "";
+  }
+  // str의 길이만큼 반복했을 때
+  for (let i = 0; i < str.length; i++) {
+    // 띄어쓰기를 제외시키기 위해 띄어쓰기가 없다면을 첫 번째 조건으로
+    if (str[i] !== " ") {
+      // 두 번째 조건으로는 값이 있는 경우
+      if (obj[str[i]] === undefined) {
+        // 값이 없는건 0으로 처리
+        obj[str[i]] = 0;
+      }
+      // 첫 번째, 두 번째 조건을 성립한다면 1씩 더한다. count + 1 처리
+      obj[str[i]]++;
+      // 가장 먼저 횟수에 도달한 문자(i)를 리턴하게 하기 위해 할당
+      if (obj[str[i]] > maxCount) {
+        maxCount = obj[str[i]];
+        frequentChar = str[i];
+      }
+    }
+  }
+  return frequentChar;
+}
+
+function mostFrequentCharacter(str) {
+  // TODO: 여기에 코드를 작성합니다.
+  //문자열 반복문 for of
+  //문자 등장 시 key = 1;
+  //이후 : +1
+  //값이 가장 큰 키를 리턴
+  let obj = {};
+  let maxCount = 0;
+  let frequentChar = "";
+
+  if (str === "") {
+    return "";
+  }
+
+  for (let s of str) {
+    if (s !== " ") {
+      if (!(s in obj)) {
+        obj[s] = 0;
+      } else {
+        obj[s]++;
+        if (obj[s] > maxCount) {
+          frequentChar = s;
+          maxCount = obj[s];
+        }
+      }
+    }
+  }
+  return frequentChar;
+}
+
+function mostFrequentCharacter(str) {
+  let obj = { maxCount: 0, frequentChar: "" };
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      continue;
+    }
+
+    if (obj[str[i]] === undefined) {
+      obj[str[i]] = 0;
+    }
+    obj[str[i]] += 1;
+
+    if (obj[str[i]] > obj["maxCount"]) {
+      obj["maxCount"] = obj[str[i]];
+      obj["frequentChar"] = str[i];
+    }
+  }
+  return obj["frequentChar"];
+}
+
 // 객체의 속성 값 조회방법
 
 // 1. dot notation
-const obj = {'name': 'elina', 'age': 30}
-console.log(obj.name)
+const obj = { name: "elina", age: 30 };
+console.log(obj.name);
+
+// dot notation 으로 객체를 할당하면, 객체 타입이 string이다
+let kimcoding = {};
+let key = "isMale";
+let value = true;
+
+kimcoding.key = value;
+// {key : true}
 
 // 2. bracket notation
-console.log(obj['name']);
+console.log(obj["name"]);
 
 // Reference Error
 console.log(obj[name]);
 // content is not defined : 정의된 content가 없다
 // obj[따옴표를 안쓰면] 변수로 인식한다
 // 새로운 변수에 obj의 속성을 할당한다면?
-let a = 'name';
+let a = "name";
 console.log(obj[a]);
 // obj 속성과 동일한 이름으로 변수를 할당한다면?
-let name = 'age';
+let name = "age";
 // obj[name]은 변수로 취급되기 때문에, false 둘을 같지 않다.
-obj[name] !== obj['name']
+obj[name] !== obj["name"];
 
 // 속성 값이 변할 수 있는 상황에는 bracket notation을 사용한다
 
 // 속성 지우기
-delete obj['name']
+delete obj["name"];
 
 // obj 안에 속성이 있는지 확인하는 방법
-'name' in obj   // true
-'email' in obj  // false
-
+"name" in obj; // true
+"email" in obj; // false
 
 // for of 는 string, array, 유사배열 등에만 사용되고, 객체에는 사용되지 않는다
 // for in 을 사용하면 object 반복문을 아주 쉽게 사용할 수 있다.
-const obj1 = {a: 1, b: 2}
-const obj2 = {b: 4, c: 3}
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 4, c: 3 };
 function ex(obj1, obj2) {
-    for (let i in obj2) {
-        if (!(i in obj1)) {
-            obj1[i] = obj2[i]
-        }
+  for (let i in obj2) {
+    if (!(i in obj1)) {
+      obj1[i] = obj2[i];
     }
-    return obj1
+  }
+  return obj1;
 }
 
 function frequentChar(str) {
-    // TODO: 여기에 코드를 작성합니다.
-    // 가장 많이 반복되고, 해당숫자에 먼저 도달한 문자
-    let frequentChar = '';
-    // 두개의 객체를 입력받아야 함으로, 빈 객체를 선언
-    let obj = {};
-    // 가장 많이 반복된 횟수를 할당 할 변수
-    let maxCount = 0;
-    // 빈 문자열을 입력받았다면 빈 문자열을 리턴
-    if (str.length === 0) {
-    return '';
-    }
-    // str의 길이만큼 반복했을 때
-    for (let i=0; i<str.length; i++) {
-      // 띄어쓰기를 제외시키기 위해 띄어쓰기가 없다면을 첫 번째 조건으로
-    if (str[i] !== ' ') {
-        // 두 번째 조건으로는 값이 있는 경우
-        if (obj[str[i]] === undefined) {
-          // 값이 없는건 0으로 처리
+  // TODO: 여기에 코드를 작성합니다.
+  // 가장 많이 반복되고, 해당숫자에 먼저 도달한 문자
+  let frequentChar = "";
+  // 두개의 객체를 입력받아야 함으로, 빈 객체를 선언
+  let obj = {};
+  // 가장 많이 반복된 횟수를 할당 할 변수
+  let maxCount = 0;
+  // 빈 문자열을 입력받았다면 빈 문자열을 리턴
+  if (str.length === 0) {
+    return "";
+  }
+  // str의 길이만큼 반복했을 때
+  for (let i = 0; i < str.length; i++) {
+    // 띄어쓰기를 제외시키기 위해 띄어쓰기가 없다면을 첫 번째 조건으로
+    if (str[i] !== " ") {
+      // 두 번째 조건으로는 값이 있는 경우
+      if (obj[str[i]] === undefined) {
+        // 값이 없는건 0으로 처리
         obj[str[i]] = 0;
-        }
-        // 첫 번째, 두 번째 조건을 성립한다면 1씩 더한다. count + 1 처리
-        obj[str[i]]++
-        // 가장 먼저 횟수에 도달한 문자(i)를 리턴하게 하기 위해 할당
-        if (obj[str[i]] > maxCount) {
+      }
+      // 첫 번째, 두 번째 조건을 성립한다면 1씩 더한다. count + 1 처리
+      obj[str[i]]++;
+      // 가장 먼저 횟수에 도달한 문자(i)를 리턴하게 하기 위해 할당
+      if (obj[str[i]] > maxCount) {
         maxCount = obj[str[i]];
-        frequentChar = str[i]
-        }
+        frequentChar = str[i];
+      }
     }
-    }  
-    return frequentChar
+  }
+  return frequentChar;
 }
 
 // 객체 메소드
 
 // Object.keys() : 객체의 키를 포함하는 배열을 만든다
 
-const a = {a:1, b:2, c:3, d:4};
-const objKeys = Object.keys(a)  // ['a', 'b', 'c', 'd']
+const a = { a: 1, b: 2, c: 3, d: 4 };
+const objKeys = Object.keys(a); // ['a', 'b', 'c', 'd']
 // forEach와 같이 사용하면 키와 값을 반복하는 데 사용할 수 있다
 Object.keys(a).forEach((key) => {
-    let value = a[key]
-    console.log(`${key}: ${value}`)
-    // a: 1
-    // b: 2
-    // c: 3
-    // d: 4
-})
+  let value = a[key];
+  console.log(`${key}: ${value}`);
+  // a: 1
+  // b: 2
+  // c: 3
+  // d: 4
+});
 // Object.keys 로 객체의 길이를 확인할 수 있다
-const length = Object.keys(a).length    // 키가 4개이므로(a,b,c,d) 4가 출력된다
+const length = Object.keys(a).length; // 키가 4개이므로(a,b,c,d) 4가 출력된다
 
 // Object.values() : 객체의 값을 포함하는 배열을 만든다
-const objValues = Object.values(a)  // [1, 2, 3, 4]
+const objValues = Object.values(a); // [1, 2, 3, 4]
 
 // Object.entries() : 객체의 키와 값으로 이루어진 중첩 배열을 만든다
-const entries = Object.entries(a)   // [["a", 1] ["b", 2] ["c", 3] ["d", 4]]
-// forEach와 같이 사용하면 결과를 반복하는 데 사용할 수 있다 
+const entries = Object.entries(a); // [["a", 1] ["b", 2] ["c", 3] ["d", 4]]
+// forEach와 같이 사용하면 결과를 반복하는 데 사용할 수 있다
 // (Object.keys 랑 entries를 이용한 거랑 동일)
 entries.forEach((entry) => {
-    let key = entry[0]
-    let value = entry[1]
-    console.log(`${key}: ${value}`)
-})
+  let key = entry[0];
+  let value = entry[1];
+  console.log(`${key}: ${value}`);
+});
 
 // Object.assign() : 객체를 다른 객체로 값을 복사할 때 사용한다
 const objAssign1 = {
-    first: 'First value',
-    second: 'Second value'
-}
+  first: "First value",
+  second: "Second value",
+};
 const objAssign2 = {
-    third: 'Third value',
-    four: 'Four value'
-}
-const assign = Object.assign(objAssign1, objAssign2)
-// { first: 'First value', 
-//   second: 'Second value', 
-//   third: 'Third value', 
+  third: "Third value",
+  four: "Four value",
+};
+const assign = Object.assign(objAssign1, objAssign2);
+// { first: 'First value',
+//   second: 'Second value',
+//   third: 'Third value',
 //   four: 'Four value'}
 
 //  스프레드 연산자를 이용하면 더 간단하게 할 수 있다.
-const assign2 = {...objAssign1, ...objAssign2}
-// { first: 'First value', 
-//   second: 'Second value', 
-//   third: 'Third value', 
+const assign2 = { ...objAssign1, ...objAssign2 };
+// { first: 'First value',
+//   second: 'Second value',
+//   third: 'Third value',
 //   four: 'Four value'}
 
 // Object.freeze() : 객체의 키와 값에 대한 수정을 방지, 추가 제거도 방지한다
