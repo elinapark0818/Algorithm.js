7031fbd1276557b753c0c3192060fc8fae111ad6
elina <elinapark0818@gmail.com>
2021-11-25 22:19:18 +0900

    Make interestCalculator

---
+++ etc/interestCalculator.js
@@ -0,0 +1,10 @@
+function interestCalculator(rate, payment, term) {
+  let interest = (((payment * term * (term + 1)) / 2) * rate) / 12;
+  console.log(interest.toFixed(0));
+}
+
+// 이율이 0.5% 일때, 매월 50만원씩 12개월 납부할 경우
+interestCalculator(0.005, 500000, 12); // 이자는 16,250원이다.
+
+// 이율이 15% 일때, 매월 60만원씩 24개월 납부할 경우
+interestCalculator(0.03, 600000, 24); // 이자는 450,000원이다.
