572009a00692d241883ba7eb4908623ae684e7cf
elina <elinapark0818@gmail.com>
2021-12-22 23:24:25 +0900

    BFS & DFS

--- study/자료구조/트리/BFS.js
+++ study/자료구조/트리/BFS.js
@@ -34,3 +34,50 @@ t.root.children[0].add("c");
 t.BFS((node) => letterBFS.push(node.data));
 
 // [‘a’, ‘b’, ‘d’, ‘c’]
+
+//
+
+// todo : "A"부터 시작한다.
+// todo : result = ["A"]
+// todo : result = ["A", "B"]
+// todo : result = ["A", "B", "C"]
+// todo : result = ["A", "B", "C", "D"]
+// todo : result = ["A", "B", "C", "D", "G"]
+// todo : result = ["A", "B", "C", "D", "G", "H"]
+// todo : result = ["A", "B", "C", "D", "G", "H", "I"]
+// todo : result = ["A", "B", "C", "D", "G", "H", "I", "E"]
+// todo : result = ["A", "B", "C", "D", "G", "H", "I", "E", "F"]
+// todo : result = ["A", "B", "C", "D", "G", "H", "I", "E", "F", "J"]
+const graph = {
+  A: ["B", "C"],
+  B: ["A", "D"],
+  C: ["A", "G", "H", "I"],
+  D: ["B", "E", "F"],
+  E: ["D"],
+  F: ["D"],
+  G: ["C"],
+  H: ["C"],
+  I: ["C", "J"],
+  J: ["I"],
+};
+
+const bfs = (graph, startNode) => {
+  let visited = []; // 탐색을 마친 노드들
+  let needVisit = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+
+  while (needVisit.length !== 0) {
+    // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    if (!visited.includes(node)) {
+      // 해당 노드가 탐색된 적 없다면
+      visited.push(node);
+      needVisit = [...needVisit, ...graph[node]];
+    }
+  }
+  return visited;
+};
+
+console.log(bfs(graph, "A"));
+// ["A", "B", "C", "D", "G", "H", "I", "E", "F", "J"]
