608d09fd321432ff88cd2ec8407f3f25db15ed40
elina <elinapark0818@gmail.com>
2022-02-07 10:05:56 +0900

    MongoDB 연습

---
+++ study/SQL/mongoDB.sql
@@ -0,0 +1,4 @@
+db.zips.insert({"_id" : ObjectId("5cBeccc1caa187d17ca6ed16"),"city" : "ALPINE","zip" : "35014","loc" : {"y" : 33.331165,"x" : 86.288934},"pop" : 3862,"state" : "AL"})
+
+db.zips.find({"city" : "ALPINE","zip" : "35014","loc" : {"y" : 33.331165,"x" : 86.288934},"pop" : 3862,"state" : "AL"})
+
