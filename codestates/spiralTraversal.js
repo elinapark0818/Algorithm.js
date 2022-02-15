488fb1a0f7eb036d5e7f243d8c266d7ed3ccab64
elina <elinapark0818@gmail.com>
2022-02-15 21:10:08 +0900

    spiralTraversal

--- codestates/spiralTraversal.js
+++ codestates/spiralTraversal.js
@@ -1,47 +1,3 @@
-const spiralTraversal = (matrix) => {
-  const M = matrix.length;
-  const N = matrix[0] && matrix[0].length;
-
-  const arr = new Array(M).fill(0).map(() => new Array(N).fill(0));
-
-  const len = M * N;
-
-  const travel = (start, m, n, spiral) => {
-    if (spiral.length === len) {
-      return spiral;
-    }
-    for (let i = start; i < n; i++) {
-      if (!arr[start][i]) {
-        spiral += matrix[start][i];
-        arr[start][i] = 1;
-      }
-    }
-    for (let i = start + 1; i < m; i++) {
-      if (!arr[i][n - 1]) {
-        spiral += matrix[i][n - 1];
-        arr[i][n - 1] = 1;
-      }
-    }
-    for (let i = n - 2; i >= start; i--) {
-      if (!arr[m - 1][i]) {
-        spiral += matrix[m - 1][i];
-        arr[m - 1][i] = 1;
-      }
-    }
-    for (let i = m - 2; i > start; i--) {
-      if (!arr[i][start]) {
-        spiral += matrix[i][start];
-        arr[i][start] = 1;
-      }
-    }
-    return travel(start + 1, m - 1, n - 1, spiral);
-  };
-
-  return travel(0, M, N, "");
-};
-
-// *
-
 // 0번째 배열은 쭈욱
 const first = (matrix, result) => {
   for (let el of matrix[0]) {
@@ -100,3 +56,12 @@ const spiralTraversal = function (matrix) {
 
   return result;
 };
+
+let matrix = [
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
+];
+
+debugger;
+spiralTraversal(matrix);
