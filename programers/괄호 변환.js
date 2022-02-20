9ff30a9f833375e122453bce8b7320f75bfc93b6
elina <elinapark0818@gmail.com>
2022-02-20 21:30:08 +0900

    괄호변환 다른코드

--- programers/괄호 변환.js
+++ programers/괄호 변환.js
@@ -40,3 +40,29 @@ function solution(p) {
     }
   }
 }
+
+// * 가장 좋아요를 많이 받은 코드
+
+function reverse(str) {
+  return str
+    .slice(1, str.length - 1)
+    .split("")
+    .map((c) => (c === "(" ? ")" : "("))
+    .join("");
+}
+
+function solution(p) {
+  if (p.length < 1) return "";
+
+  let balance = 0;
+  let pivot = 0;
+  do {
+    balance += p[pivot++] === "(" ? 1 : -1;
+  } while (balance !== 0);
+
+  const u = p.slice(0, pivot);
+  const v = solution(p.slice(pivot, p.length));
+
+  if (u[0] === "(" && u[u.length - 1] == ")") return u + v;
+  else return "(" + v + ")" + reverse(u);
+}
